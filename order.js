45c7eefe3959e6f745e1868a1f266d63bbe3b38f
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2016-04-12 22:22:55 +0200

    Inline+blocking scripts

--- order.js
+++ order.js
@@ -2,6 +2,7 @@
 
 	function groupScripts() {
 		var scripts = getAllScripts();
+		var order = 0;
 		var scriptObj = {};
 		scriptObj.async = [];
 		scriptObj.defer = [];
@@ -9,27 +10,46 @@
 		scriptObj.inline = [];
 
 		for (var i = 0; i < scripts.length; i++) {
+			order = i + 1;
 			if (scripts[i].src) {
+				// scripts with async & defer set to true is considered to be async
 				if(scripts[i].async) {
-					scriptObj.async.push(scripts[i].src);
+					scriptObj.async.push({'name': scripts[i].src, count: order });
 				} else if(scripts[i].defer) {
-					scriptObj.defer.push(scripts[i].src);
+					scriptObj.defer.push({'name': scripts[i].src, count: order });
 				} else {
-					scriptObj.blocking.push(scripts[i].src);
+					scriptObj.blocking.push({'name': scripts[i].src, count: order });
 				}
 			} else {
-				// Todo - Indentify dynamically inserted scripts
+				// Todo - Indentify dynamically inserted scripts in better way
 				if (scripts[i].innerHTML.indexOf('src') <= -1) {
-					scriptObj.inline.push(scripts[i].innerHTML);
+					scriptObj.inline.push({'name': scripts[i].innerHTML, count: order });
 				}
 			}
 		}
+		return scriptObj;
 	}
 
 	function getAllScripts() {
 		return d.getElementsByTagName('script');
 	}
 
-	groupScripts();
+	function getScriptsByType(scripts, type) {
+		return scripts[type];
+	}
+
+	function getScriptOrder() {
+		var scripts = groupScripts();
+		var inlineScripts = getScriptsByType(scripts, 'inline');
+		var blockingScripts = getScriptsByType(scripts, 'blocking');
+
+		var orderedScripts = [];
+		// Inlinescripts + blockingscripts sorted by order 
+		orderedScripts = inlineScripts.concat(blockingScripts).sort(function(a,b){return a.count > b.count});
+
+		console.table(orderedScripts);
+	}
+
+	getScriptOrder();
 
 })(window, window.document);
